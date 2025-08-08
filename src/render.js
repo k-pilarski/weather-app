42b52efadcd656c7582a77629f3cd3f9621779b3
Karol <karolpilarski04@gmail.com>
2025-08-08 17:07:38 +0200

    feat: Display dynamic weather data including temperature and a temporary icon

--- src/render.js
+++ src/render.js
@@ -2,9 +2,9 @@ import { currentWeather } from './elements.js';
 
 export function renderWeather(wData){
     currentWeather.innerHTML = '';
-    
+    let weatherIcon = '🔥';
+
     currentWeather.innerHTML += `
-        <h2>Current Weather</h2>    
-        <li>${wData.main.temp}</li>
+        <h2>Current Weather - ${wData.main.temp} ${weatherIcon}</h2>    
     `
 };
